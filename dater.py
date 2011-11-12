c43dd4592ce8d209c7d21977082ad074f59cf3ce
Brandon Reiss <brandon.l.reiss@gmail.com>
2011-11-12 15:15:40 -0500

    Change dater.py to accept dims as in   $ dater.py DIMS [FILENAME]

--- dater.py
+++ dater.py
@@ -5,9 +5,13 @@ def __main__(argv):
     """ Dater's main method. Write a weights file and exit. """
 
     # Optional command line parameter for weights filename.
-    filename = argv[1] if len(argv) > 1 else 'weights.txt'
+    if len(argv) < 2:
+        print "Usage: {0} DIMS [FILENAME]".format(argv[0])
+        return -1
+    dims = eval(argv[1])
+    filename = argv[2] if len(argv) > 2 else 'weights.txt'
     # Make a dater and write the weights.
-    D = perceptron.Dater()
+    D = perceptron.Dater(dims=dims)
     weight_lines = perceptron.Serialization.dater_to_lines(D)
     weight_file = open(filename, 'w')
     weight_file.writelines(weight_lines)
